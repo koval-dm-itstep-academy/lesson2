409e79e85dedd157f4cbd7ba317b1058ca763e28
Zerro16 <>
2020-10-08 18:43:21 +0300

    qwert

--- main.cpp
+++ main.cpp
@@ -6,11 +6,10 @@ void main() {
     cout << "Stalin watches u" << endl;
     cout << "Trotsky watces ledorub" << endl;
     cout << "Polska strong" << endl;
-<<<<<<< HEAD
+
     int a;
     cin>>a;
     cout<<a<<endl;
-=======
+
     return;
->>>>>>> 8d850434fe83765a4a4d626c2d304102a1758446
 }
