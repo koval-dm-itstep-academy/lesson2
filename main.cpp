3a26c14af92189c52b282660b83f002963791ed8
Andrey <lyp1noff@gmail.com>
2020-10-08 18:51:56 +0300

    bug fix 3

--- main.cpp
+++ main.cpp
@@ -7,12 +7,8 @@ int main()
     cout << "Stalin watches u" << endl;
     cout << "Trotsky watces ledorub" << endl;
     cout << "Polska strong" << endl;
-<<<<<<< HEAD
     cout << "Ni shagu nazad" << endl;
-<<<<<<< HEAD
-=======
 
->>>>>>> 7ed1943b2ef2b20e6c53f3ed0f4b8d2e641df017
     int a;
     cin >> a;
     cout << a << endl;
